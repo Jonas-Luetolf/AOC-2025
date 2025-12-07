e10067ed533aee7eab059f512b035a302e76bd00
Jonas-Luetolf <jonas.luetolf@outlook.com>
2025-12-07 12:20:27 +0100

    complexmatrix add cadd

--- utils/complexmatrix.py
+++ utils/complexmatrix.py
@@ -6,6 +6,10 @@ def cset(arr, c, value):
     arr[int(c.imag)][int(c.real)] = value
 
 
+def cadd(arr, c, a):
+    cset(arr, c, cindex(arr, c) + a)
+
+
 def crange(c0, c1, realstep=1, imagstep=1):
     for a in range(int(c0.imag), int(c1.imag), imagstep):
         for b in range(int(c0.real), int(c1.real), realstep):
